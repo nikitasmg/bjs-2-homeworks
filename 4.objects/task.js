ec7011f431d530a6c54fbbec37e3a8e0478d4de1
Nikita <nvsmagin@mail.ru>
2021-11-18 03:16:26 +0700

    Objects homework done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...markArr) {
+    if (this.marks === undefined) {
+        this.marks = markArr;
+    } else {
+        markArr.forEach(el => this.marks.push(el))
+    }
+}
+
+
+Student.prototype.getAverage = function() {
+    return this.marks.reduce((a, b) => a + b) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
