ab3008b7689f44e8ea5ec184d4f96ba764a99c47
Nikita <nvsmagin@mail.ru>
2021-12-03 10:09:42 +0700

    Initial commit

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,60 @@
-// тут вы можете вызывать функции из task.js
+function addNewAlarms() {
+    const timeInput = document.getElementById('time-input');
+    const textInput = document.getElementById('text-input');
+    const btn = document.getElementById('btn');
+    const btnStop = document.getElementById('btn-stop');
+    let id = 0;
+    const myNewAlarm = new AlarmClock();
+
+    const myNewCallback = (input) => {
+        const inputValue = input;
+
+
+    }
+
+    btn.addEventListener('click', () => {
+        if (timeInput.value === '' && textInput === '') {
+            event.preventDefault();
+            btn.classList.remove('btn-primary')
+            btn.classList.add('btn-danger')
+        } else {
+            event.preventDefault()
+            id += 1;
+            myNewAlarm.addClock(timeInput.value, myNewCallback, id);
+            myNewAlarm.start();
+            console.log(id)
+        }
+    })
+
+    btnStop.addEventListener('click', () => {
+        event.preventDefault();
+        myNewAlarm.stop();
+
+    })
+}
+
+
+function modalParams() {
+    const alarmModal = document.getElementById('alarm-modal');
+    const alarmModalBtnClose = document.getElementById('modal-close');
+    const alarmModalBtnOpen = document.getElementById('modal-open');
+    const alarmModalBtnCloseCross = document.getElementById('modal-close-cross')
+
+    alarmModalBtnOpen.addEventListener('click', () => {
+        alarmModal.classList.remove('closed');
+        alarmModal.classList.add('opened');
+    })
+
+    alarmModalBtnClose.addEventListener('click', () => {
+        alarmModal.classList.remove('opened');
+        alarmModal.classList.add('closed');
+    });
+
+    alarmModalBtnCloseCross.addEventListener('click', () => {
+        alarmModal.classList.remove('opened');
+        alarmModal.classList.add('closed');
+    });
+}
+
+modalParams();
+addNewAlarms();
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,64 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+    return function wrapper(...args) {
+        const key = args.join(',');
+        const idx = cache.findIndex(el => el.key === key);
+        if (idx !== -1) {
+            console.log(`Из кэша: ${cache[idx].value}`);
+            return "Из кэша: " + cache[idx].value;
+        } else if (cache.length <= 5) {
+            let result = func(...args);
+            cache.push({
+                key: key,
+                value: result,
+            });
+            console.log(`Вычисляем: ${result}`);
+            return "Вычисляем: " + result;
+        } else {
+            cache.shift();
+        }
+    }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+    let flag = false,
+        timer;
+
+    return function wrapper(...args) {
+
+        if (flag) {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                flag = false;
+            }, delay);
+            return;
+        }
+
+        func(...args);
+        flag = true;
+    }
+
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+    let flag = false,
+        timer;
+
+    function wrapper(...args) {
+        wrapper.count.push(args);
+
+        if (flag) {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                flag = false;
+            }, ms);
+            return;
+        }
+
+        func(...args);
+        flag = true;
+    }
+    wrapper.count = [];
+    return wrapper;
 }
